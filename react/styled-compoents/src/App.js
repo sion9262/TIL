96928aeffb3e3e435bc29ac3ee7f137f79d9bff0
sion <parksion9262@gmail.com>
2022-04-25 17:59:59 +0900

    [ADD] 기존 CSS 버튼 추가
    - App.css 파일 추가
    - .button, .button--color-white, button--color-black CSS 스타일 추가
    - 두 클래스를 가진 2개의 버튼 추가 (App.js)

--- react/styled-compoents/src/App.js
+++ react/styled-compoents/src/App.js
@@ -1,9 +1,10 @@
+import "./App.css"
 function App() {
   return (
     <div>
-
+      <button className="button button--color-white">하얀색</button>
+      <button className="button button--color-black">검은색</button>
     </div>
   );
 }
-
 export default App;
