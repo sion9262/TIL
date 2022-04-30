8a74b9a61af4c0ea837b005554b0fa56322d3455
sion <parksion9262@gmail.com>
2022-04-30 12:59:28 +0900

    [ADD] ThemeProvider
    - ThemeProvider 추가
    - 기존 버튼 삭제

--- react/styled-compoents/src/App.js
+++ react/styled-compoents/src/App.js
@@ -1,42 +1,29 @@
 import "./App.css"
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 
 const Wrapper = styled.section`
+  background-color: ${props => props.theme.bgColor};
+  color: ${props => props.theme.color};
   padding: 4em;
-  background: papayawhip;
   text-align: center;
 `;
 
 const Title = styled.h1`
   font-size: 1.5em;
   text-align: center;
-  color: palevioletred;
 `;
+const theme = {
+  bgColor: "black",
+  color: "white"
+}
 
-const Button = styled.button`
-  display: inline-block;
-  color: palevioletred;
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.25em 1em;
-  border: 2px solid palevioletred;
-  border-radius: 3px;
-  display: block;
-`;
-
-const TomatoButton = styled(Button)`
-  color: tomato;
-  border-color: tomato;
-`;
-
-const ReversedButton = props => <Button {...props} children={props.children.split('').reverse()} />
 function App() {
   return (
-    <Wrapper>
-      <Title>안녕하세요!</Title>
-      <Button>기본</Button>
-      <TomatoButton as={ReversedButton}>Tomato is Tomato</TomatoButton>
-    </Wrapper>
+    <ThemeProvider theme={theme}>
+      <Wrapper>
+        <Title>안녕하세요!</Title>
+      </Wrapper>
+    </ThemeProvider>
   );
 }
 export default App;
