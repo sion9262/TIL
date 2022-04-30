ac4020a374da615a87ea68623371ac9864b1f669
sion <parksion9262@gmail.com>
2022-04-30 18:22:07 +0900

    [ADD] darkmode
    - theme dark, light mode 추가

--- react/styled-compoents/src/App.js
+++ react/styled-compoents/src/App.js
@@ -1,6 +1,7 @@
-import "./App.css"
 import styled, { ThemeProvider } from "styled-components";
-import GlobalStyle from "./GlobalStyled";
+import GlobalStyle from "./GlobalStyle";
+import { useState } from "react";
+import { dark, light } from "./theme";
 
 const Wrapper = styled.section`
   background-color: ${props => props.theme.bgColor};
@@ -13,18 +14,18 @@ const Title = styled.h1`
   font-size: 1.5em;
   text-align: center;
 `;
-const theme = {
-  bgColor: "black",
-  color: "white"
-}
 
 function App() {
+  const [isDark, setIsDark] = useState(true);
+  const handleMode = () => setIsDark(!isDark);
+  const theme = isDark ? dark : light;
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle/>
       <Wrapper>
         <Title>안녕하세요!</Title>
       </Wrapper>
+      <button onClick={handleMode}>모드변경</button>
     </ThemeProvider>
   );
 }
---
+++ react/styled-compoents/src/theme.js
@@ -0,0 +1,9 @@
+export const dark = {
+	bgColor: "black",
+	color: "white"
+}
+
+export const light = {
+	bgColor: "white",
+	color: "dark"
+}
